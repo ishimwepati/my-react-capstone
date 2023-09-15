93f1652e683efd6ac8df9bca047b1c33535f1ac1
ishimwepati <wazacode@gmail.com>
2023-09-15 16:21:10 +0300

    Design added

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import './GameDetails.css'; // Import the CSS file
 
 function GameDetails() {
   const { gameID } = useParams();
@@ -21,28 +22,33 @@ function GameDetails() {
   }, [gameID, fetchGameDetails]);
 
   return (
-    <div>
+    <div className="game-details-container">
       {gameDetails ? (
         <div>
-          <h2>{gameDetails.info.title}</h2>
-          <p>
+          <h2 className="game-details-title">{gameDetails.info.title}</h2>
+          <p className="game-details-steam-app-id">
             Steam App ID:
+            {' '}
             {gameDetails.info.steamAppID}
           </p>
-          <img src={gameDetails.info.thumb} alt={gameDetails.info.title} />
-          <h3>Deals:</h3>
-          <ul>
+          <img
+            src={gameDetails.info.thumb}
+            alt={gameDetails.info.title}
+            className="game-details-image"
+          />
+          <h3 className="game-details-deals-title">Deals:</h3>
+          <ul className="game-details-deals-list">
             {gameDetails.deals.map((deal) => (
-              <li key={deal.dealID}>
-                <p data-testid="price">
+              <li key={deal.dealID} className="game-details-deal-item">
+                <p className="game-details-price">
                   Price:
-                  {gameDetails.info.price}
+                  {deal.price}
                 </p>
-                <p data-testid="retail-price">
+                <p className="game-details-retail-price">
                   Retail Price:
-                  {gameDetails.info.retailPrice}
+                  {deal.retailPrice}
                 </p>
-                <p>
+                <p className="game-details-savings">
                   Savings:
                   {deal.savings}
                   %
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,51 @@
+// Home.js
+
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchCategories } from '../actions/categoryActions';
+import './Home.css'; // Import the CSS file
 
 const Home = () => {
   const dispatch = useDispatch();
-  // Access the categories property from the categoryReducer slice of the state
   const categories = useSelector((state) => state.categoryReducer.categories);
-
-  // State to store the search query
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     dispatch(fetchCategories());
   }, [dispatch]);
 
-  // Function to handle changes in the search input
   const handleSearchInputChange = (e) => {
     setSearchQuery(e.target.value);
   };
-
-  // Function to filter games based on the search query
   // eslint-disable-next-line
   const filteredCategories = categories.filter((category) => category.external.toLowerCase().includes(searchQuery.toLowerCase()));
 
   return (
-    <div>
-      <h1>Categories</h1>
-      {/* Search input */}
-      <input
-        type="text"
-        placeholder="Search games..."
-        value={searchQuery}
-        onChange={handleSearchInputChange}
-      />
-      <ul>
+    <div className="body-class">
+      <h1>Gaming List</h1>
+      <div className="filter-container">
+        <input
+          type="text"
+          placeholder="Search games..."
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+          className="filter"
+        />
+      </div>
+      <div className="container">
         {filteredCategories.map((category) => (
-          <li key={category.gameID}>
+          <div className="grid-item" key={category.gameID}>
             <Link to={`/category/${category.gameID}`}>
               <img
                 src={category.thumb}
                 alt={category.external}
-                style={{ width: '100px', height: '100px' }}
               />
-              {category.external}
+              <h3>{category.external}</h3>
             </Link>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
