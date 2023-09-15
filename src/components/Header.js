4915effded5c87a466d1583fd5e538cc3a906ab3
ishimwepati <wazacode@gmail.com>
2023-09-15 19:48:00 +0300

    Design changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import logo from '../logo.svg';
+import logo from '../logo.png';
 import './Header.css';
 
 const Header = () => {
