f2e62ba46d7ccf4c569515108acdb7a1840c8f9b
ishimwepati <wazacode@gmail.com>
2023-09-15 16:31:21 +0300

    Header Design

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import './GameDetails.css'; // Import the CSS file
+import './GameDetails.css';
 
 function GameDetails() {
   const { gameID } = useParams();
